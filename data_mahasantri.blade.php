032196b88415fbbbb72932a0a8d5c2fbd44e5973
Aufa Zaki <107825861+faza26@users.noreply.github.com>
2023-03-03 09:28:12 +0700

    Add files via upload

---
+++ data_mahasantri.blade.php
@@ -0,0 +1,34 @@
+<h3>Data Mahasiswa</h3>
+<table border="1" cellpadding="10">
+        <tr>
+            <td><b>No</b></td>
+            <td><b>Nama</b></td>
+            <td><b>Alamat</b></td>
+        </tr>
+        <tr>
+            <td><b>1</b></td>
+            <td>{{ $mhs1 }}</td>
+            <td>{{ $asal1 }}</td>
+        </tr>
+        <tr>
+            <td><b>2</b></td>
+            <td>{{ $mhs2 }}</td>
+            <td>{{ $asal2 }}</td>
+        </tr>
+        <tr>
+            <td><b>3</b></td>
+            <td>{{ $mhs3 }}</td>
+            <td>{{ $asal3 }}</td>
+        </tr>
+        <tr>
+            <td><b>4</b></td>
+            <td>{{ $mhs4 }}</td>
+            <td>{{ $asal4 }}</td>
+        </tr>
+        <tr>
+            <td><b>5</b></td>
+            <td>{{ $mhs5 }}</td>
+            <td>{{ $asal5 }}</td>
+        </tr>
+</table>
+
